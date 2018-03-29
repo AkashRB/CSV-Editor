8118ebb1596c00e16a12bf234786153179bb8aed
AkashRB <akashrrb@gmail,com>
2018-03-29 12:31:57 +0530

    CSV Editor Hosting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, ChangeDetectorRef, ApplicationRef } from '@angular/core';
 import { PapaParseService } from 'ngx-papaparse';
 import { NgZone } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +13,12 @@ export class AppComponent {
   data = [];
   directoryData = [];
   changeDetectorRefs: ChangeDetectorRef[] = [];
+  noOfFiles: any;
   isAvailable = false;
   title = 'My CSV Editor !';
   headers = [];
   fileName: string;
+  observableBatch: any = [];
   constructor(private papa: PapaParseService, private ref: ChangeDetectorRef, private applicationRef: ApplicationRef, private zone: NgZone) {
 
   }
@@ -38,20 +41,25 @@ export class AppComponent {
   public changeListener(event: any) {
     let self = this;
     this.data = [];
-    //for (var i = 0; i < event.target.files.length; i++) {
-      this.fileName = event.target.files[0].name.split(".")[0];
+    this.noOfFiles = event.target.files.length;
+    for (var i = 0; i < event.target.files.length; i++) {
+      this.fileName = event.target.files[i].name.split(".")[0];
       {
-        let file: File = event.target.files.item(0);
+        let file: File = event.target.files.item(i);
+        console.log(file);
+        
+        let fileData: File;
         let reader: FileReader = new FileReader();
-        reader.onloadend = function (event) {
-          self.parseCSVFile(reader.result);
-        }
         reader.readAsText(file);
+        let fileReaderPromise = new Promise(resolve => reader.onload = resolve);
+         fileReaderPromise.then(e => {
+          this.parseCSVFile(reader.result);
+          console.log("All files data", this.data);
+          this.fillTheColumnHeader();
+        });
       }
-    
-    console.log("All files data", this.data);
-    this.fillTheColumnHeader();
-  }
+    }
+  }  
   /**
    * Method to parse the csv File
    * @param {*} data 
